04bf5e223928be69141d9d357f993110d01758bb
Hurn2308 <149349344+Hurn2308@users.noreply.github.com>
2024-04-09 17:33:40 +0800

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -4,12 +4,12 @@ int main(){
   int i;
 
   for (i = 0; i <= 100; i++){
-    if (i % 3 == 0){
-      std::cout << "Fizz!\n";
+    if (i % 15 == 0){
+      std::cout << "FizzBuzz!\n";
     } else if(i % 5 == 0){
       std::cout << "Buzz!\n";
-    } else if (i % 3 == 0 && i % 5 == 0){
-      std::cout << "FizzBuzz!\n";
+    } else if (i % 3 == 0){
+      std::cout << "Fizz!\n";
     } else {
     std::cout << i << "!\n";
     }
