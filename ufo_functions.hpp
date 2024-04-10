2dad622416ed312b9987d2e115db3bc8ae76b567
Hurn2308 <149349344+Hurn2308@users.noreply.github.com>
2024-04-10 21:50:20 +0800

    Delete ufo_functions.hpp

--- ufo_functions.hpp
+++
@@ -1,6 +0,0 @@
-#include <vector>
-void greet();
-void game();
-void end_game(std::string answer, std::string codeword);
-void display_misses(int misses);
-void display_status(std::vector<char> incorrect, std::string answer);
